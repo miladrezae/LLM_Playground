3d9da4b5bb2294fea5e2d9384711292773b46749
unknown <milad.rezaeshirazi@gmail.com>
2024-01-28 01:32:32 +0330

    Back

--- openai_chatgpt_API.py
+++ openai_chatgpt_API.py
@@ -1,5 +1,5 @@
 # Chat with an intelligent assistant in your terminal
-from openai import OpenAI
+from openai import OpenAI, api_key
 from prompts.contexts import context_document, biography
 from prompts.instructs import instructions
 from libs.helper import num_tokens_from_string
@@ -8,7 +8,9 @@ from settings.open_ai_api import OPEN_AI_SECRET
 seed_number = 42
 welcome_message = "Hello, introduce yourself as Artificial Milad using 2-3 sentences."
 
-client = OpenAI(base_url="https://api.openai.com/v1/chat/completions", api_key=OPEN_AI_SECRET,)#Read at #https://platform.openai.com/docs/api-reference/chat/create
+client = OpenAI(base_url="https://api.openai.com/v1/chat/completions", api_key=OPEN_AI_SECRET,)#Read at https://platform.openai.com/docs/api-reference/chat/create
+#Example at https://gist.github.com/pszemraj/c643cfe422d3769fd13b97729cf517c5
+api_key=OPEN_AI_SECRET
 
 history = [
     {"role": "system", "content": f""" Answer technical work questions based on the following:\n {context_document} \n
@@ -20,8 +22,8 @@ history = [
 print(str(num_tokens_from_string(history[0]['content'], "cl100k_base"))+" Tokens!")
 
 while True:
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo-instruct", 
+    completion = client.ChatCompletion.create(
+        model="gpt-3.5-turbo",#"gpt-3.5-turbo-instruct" 
         stop = ["###", "user:", "assistant:", "instruction:", "###instruction:", "Instruction:", "User:", "Instructions:"],
         messages=history,
         temperature=0.3,
